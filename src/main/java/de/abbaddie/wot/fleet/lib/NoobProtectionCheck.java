cf317faf230758a017f9ee3e652d05244f473e35
Biggerskimo <git@abbaddie.de>
2012-03-30 23:21:24 +0200

    Aktivität beim Noobschutz closes #7

--- src/main/java/de/abbaddie/wot/fleet/lib/NoobProtectionCheck.java
+++ src/main/java/de/abbaddie/wot/fleet/lib/NoobProtectionCheck.java
@@ -1,5 +1,8 @@
 package de.abbaddie.wot.fleet.lib;
 
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+
 import de.abbaddie.wot.data.user.User;
 
 public final class NoobProtectionCheck {
@@ -15,6 +18,9 @@ public final class NoobProtectionCheck {
 		long ofiaraPoints = ofiara.getStatEntry().getPoints();
 		
 		long limitPoints = (long) (ofiaraPoints * (Math.pow(ofiaraPoints, 3) + 2));
-		return ownerPoints > limitPoints;
+		
+		boolean active = ofiara.getLastActivity().isAfter(DateTime.now().minus(new Period().withWeeks(1)));
+		
+		return ownerPoints > limitPoints && active;
 	}
 }
